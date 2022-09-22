3b75398ba0e8d4c50c1e2f2face1985e04f019c0
juancorredor <juansc619@hotmail.com>
2022-09-22 13:14:24 -0500

    V1

---
+++ src/main/java/com/example/holaSpring/Controlador.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.holaSpring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author lore-
+ */
+@RestController
+public class Controlador {
+    @GetMapping("/")
+    public String mensaje(){
+        return "Hola mundo";
+    }
+}
